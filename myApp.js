8016561377bffb00c2200b66dfecad74793561af
Hugo Suárez <hugolgsuarez@gmail.com>
2022-07-21 10:50:57 +0000

    Static server

--- myApp.js
+++ myApp.js
@@ -1,10 +1,16 @@
 let express = require('express');
 let app = express();
+const path = require("path");
+
+
+const absPath = path.join(__dirname, "./views/index.html");
 
 
 
-
-
+const handler=(req,res)=>{
+  res.sendFile(absPath)
+}
+app.get('/',handler)
 
 
 
