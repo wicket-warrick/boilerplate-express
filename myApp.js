f925bd9dbc1b3e077a11e5241271411c7b348f36
Hugo Suárez <hugolgsuarez@gmail.com>
2022-07-21 21:59:11 +0000

    various

--- myApp.js
+++ myApp.js
@@ -5,14 +5,19 @@ const path = require("path");
 
 const absPath = path.join(__dirname, "./views/index.html");
 
-
-
 const handler=(req,res)=>{
   res.sendFile(absPath)
 }
+app.use("/public", express.static(__dirname + "/public"));
+
+
+
+
+
 app.get('/',handler)
-
-
+app.get("/json",function (req,res){
+  res.json({"message":"continuamos aprendendo"})
+})
 
 
 
